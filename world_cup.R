64767629a70420803af7e712229051ffc37bb86b
Luke Benz <luke.benz@yale.edu>
2018-06-19 22:44:22 -0400

    Update 6/19/18

--- world_cup.R
+++ world_cup.R
@@ -104,17 +104,17 @@ y <- rbind(y, fixtures %>% mutate("tournament" = "World Cup", "neutral" = locati
                                   "date" = as.Date(date, "%m/%d/%y"),
                                   "days_since" = as.numeric(Sys.Date() - date), 
                                   "game_type" = "WC",
-                                  "match_weight"= 4) %>% 
+                                  "match_weight"= 6) %>% 
              select(team, opponent, tournament, neutral, goals,days_since, date, location, 
                     game_type, match_weight) %>%
              filter(days_since >= 0))
 
 y <- rbind(y, invert(fixtures, T) %>% mutate("tournament" = "World Cup", "neutral" = location == "N", 
-                                          "goals" = team_score,
-                                          "date" = as.Date(date, "%m/%d/%y"),
-                                          "days_since" = as.numeric(Sys.Date() - date), 
-                                          "game_type" = "WC",
-                                          "match_weight"= 4) %>% 
+                                             "goals" = team_score,
+                                             "date" = as.Date(date, "%m/%d/%y"),
+                                             "days_since" = as.numeric(Sys.Date() - date), 
+                                             "game_type" = "WC",
+                                             "match_weight"= 6) %>% 
              select(team, opponent, tournament, neutral, goals,days_since, date, location, 
                     game_type, match_weight) %>%
              filter(days_since >= 0))
@@ -168,24 +168,33 @@ sim_group <- function(group_name) {
     "team_goals" = NA, "opp_goals" = NA, "team_gd" = NA, "opp_gd" = NA,
     "team_points" = 0, "opp_points" = 0)
   for(i in 1:nrow(games)) {
-    lambda_1 <- games$team_score[i]
-    lambda_2 <- games$opponent_score[i]
-    games$team_goals[i] <- rpois(1, lambda_1)
-    games$opp_goals[i] <- rpois(1, lambda_2)
-    games$team_gd[i] <- games$team_goals[i] - games$opp_goals[i]
-    games$opp_gd[i] <- games$opp_goals[i] - games$team_goals[i]
-    if(games$team_gd[i] > 0) {
-      games$team_points[i] <- 3
+    if(games$win[i] == 1 | games$loss[i] == 1 | games$tie[i] == 1) {
+      games$team_goals[i] <- games$team_score[i]
+      games$opp_goals[i] <- games$opponent_score[i]
+      games$team_gd[i] <- games$goal_diff[i]
+      games$opp_gd[i] <- -games$goal_diff[i]
+      games$team_points[i] <- 3*games$win[i] + games$tie[i]
+      games$opp_points[i] <- 3*games$loss[i] + games$tie[i]
     }
-    if(games$opp_gd[i] > 0) {
-      games$opp_points[i] <- 3
-    }
-    if(games$team_gd[i] == 0) {
-      games$team_points[i] <- 1
-      games$opp_points[i] <- 1
+    else{
+      lambda_1 <- games$team_score[i]
+      lambda_2 <- games$opponent_score[i]
+      games$team_goals[i] <- rpois(1, lambda_1)
+      games$opp_goals[i] <- rpois(1, lambda_2)
+      games$team_gd[i] <- games$team_goals[i] - games$opp_goals[i]
+      games$opp_gd[i] <- games$opp_goals[i] - games$team_goals[i]
+      if(games$team_gd[i] > 0) {
+        games$team_points[i] <- 3
+      }
+      if(games$opp_gd[i] > 0) {
+        games$opp_points[i] <- 3
+      }
+      if(games$team_gd[i] == 0) {
+        games$team_points[i] <- 1
+        games$opp_points[i] <- 1
+      }
     }
   }
-  
   ladder <- data.frame("country" = unique(c(games$team, games$opponent)),
                        "pts" = rep(NA, 4),
                        "goals_forced" = rep(NA, 4),
@@ -266,7 +275,7 @@ for(k in 1:nsims) {
     knockout$opp_goals <- 
       predict(glm.futbol, newdata = invert(knockout), type = "response")
     
-
+    
     winners <- rep(NA, teams_left/2)
     for(i in 1:nrow(knockout)) {
       team_goals <- rpois(1, knockout$team_goals[i])
@@ -305,47 +314,81 @@ write.csv(wc_sims, "wc_sims.csv", row.names = F)
 
 ### Goal Plot Function
 goal_plot <- function(team1, team2, location, col1, col2) {
- lambda1 <- predict(glm.futbol, newdata = data.frame("team" = team1,
-                                          "opponent" = team2,
-                                          "location" = location,
-                                          stringsAsFactors = F), 
-                   type = "response")
- lambda2 <- predict(glm.futbol, newdata = invert(data.frame("team" = team1,
-                                          "opponent" = team2,
-                                          "location" = location,
-                                          stringsAsFactors = F)), 
-                   type = "response")
- max_goals <- 10000
- score_matrix <- dpois(0:max_goals, lambda1) %o% dpois(0:max_goals, lambda2)
- tie_prob <- sum(diag(score_matrix))
- win_prob <- sum(score_matrix[lower.tri(score_matrix)])
- loss_prob <- sum(score_matrix[upper.tri(score_matrix)])
- 
- 
- z <- data.frame("Team" = rep(c(team1, team2), rep(4,2)),
-                 "Goals" = rep(c("0", "1", "2", "3+"), 2),
-                 "Probability" = c(dpois(0:2, lambda1), sum(dpois(3:10, lambda1)),
-                             dpois(0:2, lambda2), sum(dpois(3:10, lambda2))))
- 
- vec <- c(team1, team2)
- vec <- sort(vec)
- if(vec[1] == team2) {
-   tmp <- col1
-   col1 <- col2
-   col2 <- tmp
- }
- ggplot(z, aes(x = Goals, y = Probability, fill = Team)) + 
-   geom_bar(stat = "identity", position='dodge', colour  = "black") + 
-   labs(title = paste(team1, "vs.", team2, "Goal Distributions")) + 
-   scale_fill_manual(values=c(col1, col2)) +
-   annotate("text", x = 3.4, y = 0.4, label = paste(team1, "Expected Goals:", round(lambda1, 2))) +
-   annotate("text", x = 3.4, y = 0.39, label = paste(team1, "Win Probability", round(win_prob, 2))) + 
-   annotate("text", x = 3.4, y = 0.38, label = paste(team2, "Expected Goals:", round(lambda2, 2))) + 
-   annotate("text", x = 3.4, y = 0.37, label = paste(team2, "Win Probability", round(loss_prob, 2))) + 
-   annotate("text", x = 3.4, y = 0.36, label = paste("Tie Probability", round(tie_prob, 2)))
- 
+  lambda1 <- predict(glm.futbol, newdata = data.frame("team" = team1,
+                                                      "opponent" = team2,
+                                                      "location" = location,
+                                                      stringsAsFactors = F), 
+                     type = "response")
+  lambda2 <- predict(glm.futbol, newdata = invert(data.frame("team" = team1,
+                                                             "opponent" = team2,
+                                                             "location" = location,
+                                                             stringsAsFactors = F)), 
+                     type = "response")
+  max_goals <- 10
+  score_matrix <- dpois(0:max_goals, lambda1) %o% dpois(0:max_goals, lambda2)
+  tie_prob <- sum(diag(score_matrix))
+  win_prob <- sum(score_matrix[lower.tri(score_matrix)])
+  loss_prob <- sum(score_matrix[upper.tri(score_matrix)])
+  
+  
+  z <- data.frame("Team" = rep(c(team1, team2), rep(4,2)),
+                  "Goals" = rep(c("0", "1", "2", "3+"), 2),
+                  "Probability" = c(dpois(0:2, lambda1), sum(dpois(3:10, lambda1)),
+                                    dpois(0:2, lambda2), sum(dpois(3:10, lambda2))))
+  
+  vec <- c(team1, team2)
+  vec <- sort(vec)
+  if(vec[1] == team2) {
+    tmp <- col1
+    col1 <- col2
+    col2 <- tmp
+  }
+  ggplot(z, aes(x = Goals, y = Probability, fill = Team)) + 
+    geom_bar(stat = "identity", position='dodge', colour  = "black") + 
+    labs(title = paste(team1, "vs.", team2, "Goal Distributions")) + 
+    scale_fill_manual(values=c(col1, col2)) +
+    annotate("text", x = 3.4, y = 0.4, label = paste(team1, "Expected Goals:", round(lambda1, 2))) +
+    annotate("text", x = 3.4, y = 0.39, label = paste(team1, "Win Probability", round(win_prob, 2))) + 
+    annotate("text", x = 3.4, y = 0.38, label = paste(team2, "Expected Goals:", round(lambda2, 2))) + 
+    annotate("text", x = 3.4, y = 0.37, label = paste(team2, "Win Probability", round(loss_prob, 2))) + 
+    annotate("text", x = 3.4, y = 0.36, label = paste("Tie Probability", round(tie_prob, 2)))
+  
 }
 
-goal_plot("Sweden", "Korea Republic", "N", "gold1", "darkslateblue")
-goal_plot("Belgium", "Panama", "N", "black", "red2")
-goal_plot("Tunisia", "England", "N", "red2", "powderblue")
+goal_jgp <- function(team1, team2, location) {
+  lambda1 <- predict(glm.futbol, newdata = data.frame("team" = team1,
+                                                      "opponent" = team2,
+                                                      "location" = location,
+                                                      stringsAsFactors = F), 
+                     type = "response")
+  lambda2 <- predict(glm.futbol, newdata = invert(data.frame("team" = team1,
+                                                             "opponent" = team2,
+                                                             "location" = location,
+                                                             stringsAsFactors = F)), 
+                     type = "response")
+  t1gprob <- dpois(0:5, lambda1)
+  t2gprob <- dpois(0:5, lambda2)
+  df <- expand.grid(t1gprob, t2gprob)
+  df2 <- expand.grid(0:5, 0:5)
+  names(df) <- c("t1gprob", "t2gprob")
+  names(df2) <- c("t1g", "t2g")
+  df <- cbind(df, df2)
+  ggplot(df, aes(x = t1g, y = t2g, fill = t1gprob * t2gprob)) + 
+    geom_raster(alpha  = 0.9) + 
+    scale_fill_gradient2(low = "navy", mid = "white", midpoint = 0.05,  high = "red") + 
+    labs(x = paste(team1, "Goals"), y = paste(team2, "Goals"), fill = "Joint\nProbability",
+         title = paste(team1, "vs.", team2, "Joint Goal Probability Distribution")) + 
+    scale_x_continuous(waiver(), breaks = 0:5, labels = as.character(0:5)) + 
+    scale_y_continuous(waiver(), breaks = 0:5, labels = as.character(0:5))
+}
+
+goal_jgp("Portugal", "Morocco", "N")
+goal_jgp("Uruguay", "Saudi Arabia", "N")
+goal_jgp("Spain", "Iran", "N")
+
+
+goal_plot("Portugal", "Morocco", "N", "aquamarine1", "red4")
+goal_plot("Spain", "Iran", "N", "red2", "forestgreen")
+goal_plot("Uruguay", "Saudi Arabia", "N", "skyblue1", "forestgreen")
+
+
